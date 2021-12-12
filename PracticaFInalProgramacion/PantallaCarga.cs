c4312662c11526a014fbcd87fc63df8371b5b5ee
SmaylinRobles <smaylinrobles@gmail.com>
2021-12-11 23:03:49 -0400

    Mis cambios

---
+++ PracticaFInalProgramacion/PantallaCarga.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaFInalProgramacion
+{
+    public partial class PantallaCarga : Form
+    {
+        public PantallaCarga()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(2);
+            label1.Text = progressBar1.Value.ToString() + "%";
+
+                if(progressBar1.Value == progressBar1.Maximum)
+            {
+                timer1.Stop();
+                this.Hide();
+                FormLogin frmLogin = new FormLogin();
+                frmLogin.ShowDialog();
+            }
+        }
+    }
+}
